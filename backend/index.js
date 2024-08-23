4ad153696860a4dae1ff9ca97c405dfdcdce6663
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-08-23 14:32:34 +0330

    ""

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import baseInfo from "./routes/baseInfo.js";
 
-
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', baseInfo);
+app.use("/api", baseInfo);
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`Listening on port http://localhost:${port}`)
