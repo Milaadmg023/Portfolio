b2a5cd081f5d2de1b5e040e04631d321903534eb
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-08-23 11:34:10 +0330

    adding static files

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import baseInfo from "./routes/baseInfo.js";
+
+
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', baseInfo);
