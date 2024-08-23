45a8659a7a3db007c1b98c554c25043a70c03e7b
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-08-23 11:01:20 +0330

    adding about & aside page

---
+++ frontend/src/components/main/main_content.jsx
@@ -0,0 +1,10 @@
+import About from "./about";
+import Navbar from "./navbar";
+export default function Main_Content() {
+  return (
+    <div className="main-content">
+      <Navbar />
+      <About/>
+    </div>
+  );
+}
