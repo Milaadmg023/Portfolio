5333f8c0918ddc4d5e35840ca13c9e7371c99e1d
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-10-21 10:45:04 +0330

    vercel

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,15 +1,7 @@
 export default async function fetcher(url) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
-  const fullUrl = new URL(url, apiUrl);
   try {
-    const res = await fetch(`${apiUrl}${url}`);
-    if (contentType && contentType.includes("application/json")) {
-      return response.json();
-    } else {
-      const text = await response.text();
-      console.error('Unexpected response:', text);
-      throw new Error('Invalid JSON response');
-    }
+    const res = await fetch(url);
+    return res.json();
   } catch (error) {
     console.log(error);
     return error;
--- utils/message.js
+++ utils/message.js
@@ -1,21 +1,13 @@
 export default async function Send_message(url, data) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
-  const fullUrl = new URL(url, apiUrl);
   try {
-    const res = await fetch(fullUrl, {
+    const res = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    if (contentType && contentType.includes("application/json")) {
-      return response.json();
-    } else {
-      const text = await response.text();
-      console.error('Unexpected response:', text);
-      throw new Error('Invalid JSON response');
-    }
+    return res.json();
   } catch (error) {
     console.log(error);
     return error;
