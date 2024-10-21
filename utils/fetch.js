0ecfa81274bc0fd656497e74bde55d077c6bf361
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-10-21 10:59:52 +0330

    vercel

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,7 @@
 export default async function fetcher(url) {
+  const baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
   try {
-    const res = await fetch(url);
+    const res = await fetch(baseUrl + url);
     return res.json();
   } catch (error) {
     console.log(error);
--- utils/message.js
+++ utils/message.js
@@ -1,6 +1,7 @@
 export default async function Send_message(url, data) {
+  const baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
   try {
-    const res = await fetch(url, {
+    const res = await fetch(baseUrl + url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
