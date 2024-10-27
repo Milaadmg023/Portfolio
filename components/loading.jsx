f0c77f498cbd07370172ebb8537ee76e03ec2df8
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-10-27 11:05:32 +0330

    adding loading to home page

--- components/loading.jsx
+++ components/loading.jsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export default function Loader() {
   return (
     <div className="h-screen flex items-center justify-center bg-white fixed top-0 left-0 right-0 bottom-0">
       <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
