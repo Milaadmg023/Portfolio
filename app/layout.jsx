3c3060bc700f11ce4770911dfce44771782abed4
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-10-27 10:33:07 +0330

    edititng metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,6 @@ import "./globals.css";
 export const metadata = {
   title: "Portfolio",
   description: "Created by Milad Bageri | https://t.me/CodinaDev",
-  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({ children }) {
